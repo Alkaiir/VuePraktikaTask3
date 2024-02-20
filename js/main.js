a57a8b87508fa26f35c6944296105ff7a4ee9939
Alkaiir <ivv.ven0@gmail.com>
2024-02-20 14:40:59 +0700

    Project Create

---
+++ js/main.js
@@ -0,0 +1,27 @@
+Vue.component('board', {
+    template: `
+    <div>
+    
+    </div>
+ `,
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+    }
+
+})
+
+
+
+let eventBus = new Vue()
+
+
+let app = new Vue({
+    el: '#app',
+})
